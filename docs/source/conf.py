a74538b117a8a1f15d77d1451b8a20960e4f6ce1
tongshuangwu <wtshuang@cs.uw.edu>
2020-05-06 11:04:14 -0700

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../checklist'))
 
 autodoc_mock_imports = [
